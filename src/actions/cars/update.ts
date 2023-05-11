51adc401df56943ff0a2c0a153399e6de9660883
Gregorio Rampche <gregorioalmeida@gmail.com>
2023-05-11 19:46:47 +0100

    final updates to cars actions.

--- src/actions/cars/update.ts
+++ src/actions/cars/update.ts
@@ -2,15 +2,15 @@ import { Request, Response } from 'express';
 import { update, read } from '../../services/cars';
 
 export default async (req: Request, res: Response) => {
-  const { car_id } = req.params;
+  const { id } = req.params;
 
-  if (!(await read(car_id))) {
+  if (!(await read(id))) {
     return res.status(404).json({
       code: 404,
       message: 'Car not found',
     });
   }
 
-  const car = await update(car_id, req.body);
+  const car = await update(id, req.body);
   return res.json(car);
 };
